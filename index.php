f4e84d2341e95cae4e9480a8103553be3613cd29
Muhammad Sa'id Najih <ininajih@gmail.com>
2025-03-04 22:35:55 +0700

    tugas 2

---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Bootstrap demo</title>
+  <link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+    crossorigin="anonymous" />
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Muhammad Sa'id Najih</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contact.php">Contact</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="segitiga.php">Segitiga</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="kubus.php">Kubus</a>
+          </li>
+      </div>
+    </div>
+  </nav>
+  <div class="judul">
+    <h1>
+      <center>Curiculum Vitae</center>
+    </h1><br>
+    <img src="profil.jpg" class="rounded mx-auto d-block" alt="foto" width="200px">
+  </div><br><br>
+  <div class="container-sm">
+    <center>
+      <h2>Biodata</h2>
+      <table class="table table-hover" border="0">
+        <ul>
+          <tr>
+            <td>
+              <h5>
+                <li>Nama</li>
+              </h5>
+            </td>
+            <td>
+              <h5> : Muhammad Sa'id Najih</h5>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <h5>
+                <li>TTL</li>
+              </h5>
+            </td>
+            <td>
+              <h5> : Semarang, 3 Oktober 2005</h5>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <h5>
+                <li>Alamat</li>
+              </h5>
+            </td>
+            <td>
+              <h5> : Desa Tuntang Kecamatan Tuntang Kabupaten Semarang</h5>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <h5>
+                <li>Asal Univesitas</li>
+              </h5>
+            </td>
+            <td>
+              <h5> : UPN Veteran Yogyakarta</h5>
+            </td>
+          </tr>
+        </ul>
+      </table>
+    </center>
+  </div>
+  <div class="card-footer text-body-secndary text-end">
+    <h6>Yogyakarta, 20 Desember 2025</h6>
+  </div>
+
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
